a4d8cbd5ffbf282ce5c3287d2c2e1ff11c37e80e
Anusri Karmokar <142775407+anusrimk@users.noreply.github.com>
2024-06-23 13:43:08 +0530

    Add files via upload

---
+++ Day_10/semaphore.c
@@ -0,0 +1,33 @@
+/*Semaphore*/
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+int main(){
+    int flags, sval;
+    unsigned int value;
+    sem_t *sem;
+
+    sem = sem_open("/sem1", O_CREAT | O_RDWR, 0660, 2);
+    if(sem == SEM_FAILED){
+        perror("sem_open fail;");
+        return -1;
+    }
+    else{
+        printf("\n sem_open success\n");
+    }
+
+    sem_getvalue(sem, &sval);
+    printf("\n before sem_wait semaphore value = (%d)\n, sval");
+
+    sem_wait(sem);
+    sem_getvalue(sem, &sval);
+    printf("\n after sem_wait semaphore value =(%d)\n, sval");
+
+    printf("\nprocess 1: Executing Critical Section\n");
+    sleep(20);
+
+}
